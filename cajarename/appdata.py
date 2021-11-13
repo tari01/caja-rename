2c15e224182511d4d9caab822d88b57596a609be
Robert Tari <robert@tari.in>
2021-11-13 22:45:12 +0100

    Release 21.11.13

--- cajarename/appdata.py
+++ cajarename/appdata.py
@@ -23,7 +23,7 @@
 APPNAME = 'cajarename'
 APPEXECUTABLE='/usr/share/caja-python/extensions/caja-rename.py'
 APPDEPENDENCIES = ['python3-gi', 'gir1.2-gtk-3.0', 'caja']
-APPVERSION = '21.7.18'
+APPVERSION = '21.11.13'
 APPSHOWSETTINGS = 170323
 APPYEAR = '2017'
 APPTITLE = 'Rename...'
